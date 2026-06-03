73c070a952a8c4040713e9c1818d447cb5ef7013
johnpathadan <johnpathadan@gmail.com>
2026-06-03 13:13:31 +0530

    nav bar added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 export default function RootLayout({
   children,
@@ -6,11 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`h-full antialiased bg-black`}
-    >
-      <body className="min-h-full flex flex-col text-white m-12">{children}</body>
+    <html lang="en" className={`h-full antialiased bg-black`}>
+      <body className="min-h-full flex flex-col text-white m-12">
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-amber-100 rounded-xl px-4 py-3 sm:px-6 md:px-8">
+      <div className="max-w-7xl mx-auto">
+        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-black text-black">
+          Token Lab
+        </p>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
